375d085567851df8a789504590f451a9d17a97ce
Richard Huang <flyerhzm@gmail.com>
2018-08-04 18:48:44 +0800

    update deploy.sh

--- DOCKER.md
+++ DOCKER.md
@@ -4,4 +4,4 @@ docker push xinminlabs/awesomecode-docs:$(git rev-parse --short HEAD)
 
 docker service create --name awesomecode-docs --publish mode=host,target=80,published=10080 xinminlabs/awesomecode-docs:$(git rev-parse --short HEAD)
 
-docker service update --name awesomecode-docs xinminlabs/awesomecode-docs:$(git rev-parse --short HEAD)
+docker service update --image xinminlabs/awesomecode-docs:$(git rev-parse --short HEAD) awesomecode-docs
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker build -f Dockerfile.prod -t xinminlabs/awesomecode-docs:$(git rev-parse --short HEAD) .
 docker push xinminlabs/awesomecode-docs:$(git rev-parse --short HEAD)
 
-result=$(ssh -o StrictHostKeyChecking=no xinminlabs.com "docker service update --name awesomecode xinminlabs/awesomecode-docs:$(git rev-parse --short HEAD)")
+result=$(ssh -o StrictHostKeyChecking=no xinminlabs.com "docker service update --image xinminlabs/awesomecode-docs:$(git rev-parse --short HEAD) awesomecode-docs")
 [ $? -gt 0 ] && echo 'deployment failed' && exit 1
 echo $result
