5d4e38bf881b27ae619d4b725eb1e35b6f5eb225
Richard Huang <flyerhzm@gmail.com>
2021-09-24 07:15:55 +0800

    rm deploy.sh

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-docker build -f Dockerfile.prod -t xinminlabs/awesomecode-docs:$(git rev-parse --short HEAD) .
-docker push xinminlabs/awesomecode-docs:$(git rev-parse --short HEAD)
-
-result=$(ssh -o StrictHostKeyChecking=no xinminlabs.com "docker service update --image xinminlabs/awesomecode-docs:$(git rev-parse --short HEAD) awesomecode-docs")
-[ $? -gt 0 ] && echo 'deployment failed' && exit 1
-echo $result
